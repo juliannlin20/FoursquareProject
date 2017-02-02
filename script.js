5f6e74bce9f69bb72e414ec23d065caa8e99f6f5
juliannlin20 <julin@cristoreyny.org>
2017-02-02 12:59:07 +0000

    my message

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// JavaScript File
+$(document).ready(function(){
+    $("#search").click(function(){
+       var searchVal1 = $("#location").val();
+       var searchVal2 = $("#food").val();
+       var part1= "https://api.foursquare.com/v2/venues/search?v=20161016&near="
+       var part2= "&query="
+       var part3= "&intent=checkin&client_id=2WXQFD3HEVCBIYPT2JXGUU45GODUQU2PP4WMZXNTTXNO4VBX&client_secret=QEAA0PIPMCERJ4UY0BMAXUJZHSSI3PPHDEXG1U2CWUACHAYH"
+       var url= part1 + searchVal1 + part2 + searchVal2 + part3;
+       console.log(url);
+       $.getJSON(url, function(data){
+           console.log(data);
+           var info= data.response.venues.name
+           alert(info);
+           
+          
+    })
+})
